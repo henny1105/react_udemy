0e8ddace2aaed0117d74c4cf21b4b1d6b0f5628b
henny <kimhyein1105@gmail.com>
2024-01-09 22:03:29 +0900

    동적 HTML Attributes(속성) 설정 & 이미지 파일 로딩

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,7 @@
+import reactImg from './assets/react-core-concepts.png';
+
 const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
+
 function genRandomInt(max) {
 	return Math.floor(Math.random() * (max + 1));
 }
@@ -8,7 +11,9 @@ function Header() {
 	const description = reactDescriptions[genRandomInt(2)];
 	return (
 		<header>
-			<img src='src/assets/react-core-concepts.png' alt='Stylized atom' />
+			{/* 리액트로 불러올 때 이미지를 유실할 수도 있음 */}
+			{/* <img src='src/assets/react-core-concepts.png' alt='Stylized atom' /> */}
+			<img src={reactImg} alt='Stylized atom' />
 			<h1>React Essentials</h1>
 			<p>
 				{/* 중괄호 사이에 자유롭게 자바스크립트 코드 실행 */}
