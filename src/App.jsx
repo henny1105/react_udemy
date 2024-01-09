b92c88030e605459ff6b2c372934363d0da269f8
henny <kimhyein1105@gmail.com>
2024-01-09 22:15:53 +0900

    Prop(속성)으로 컴포넌트 재사용

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,19 @@
 import reactImg from './assets/react-core-concepts.png';
+import componentsImg from './assets/components.png';
 
 const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
 
 function genRandomInt(max) {
 	return Math.floor(Math.random() * (max + 1));
 }
-// 랜덤으로 텍스트값 가져오기
 
 function Header() {
 	const description = reactDescriptions[genRandomInt(2)];
 	return (
 		<header>
-			{/* 리액트로 불러올 때 이미지를 유실할 수도 있음 */}
-			{/* <img src='src/assets/react-core-concepts.png' alt='Stylized atom' /> */}
 			<img src={reactImg} alt='Stylized atom' />
 			<h1>React Essentials</h1>
 			<p>
-				{/* 중괄호 사이에 자유롭게 자바스크립트 코드 실행 */}
 				{description}
 				React concepts you will need for almost any app you are going to build!
 			</p>
@@ -24,10 +21,31 @@ function Header() {
 	);
 }
 
+function CoreConcept(props) {
+	return (
+		<li>
+			<img src={props.img} alt={props.title} />
+			<h3>{props.title}</h3>
+			<p>{props.description}</p>
+		</li>
+	);
+}
+
 function App() {
 	return (
 		<div>
 			<Header />
+			<main>
+				<section id='core-concepts'>
+					<h2>Core Concepts</h2>
+					<ul>
+						<CoreConcept title='Components' description='The core UI building block' img={componentsImg} />
+						<CoreConcept />
+						<CoreConcept />
+						<CoreConcept />
+					</ul>
+				</section>
+			</main>
 			<main>
 				<h2>Time to get started!</h2>
 			</main>
