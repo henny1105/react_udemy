7e0551487c8e4ab99f0cf1144c3595c5067058f1
henny <kimhyein1105@gmail.com>
2024-01-09 21:57:53 +0900

    동적 값 출력 및 활용

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
 const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
-
 function genRandomInt(max) {
 	return Math.floor(Math.random() * (max + 1));
 }
+// 랜덤으로 텍스트값 가져오기
 
 function Header() {
+	const description = reactDescriptions[genRandomInt(2)];
 	return (
 		<header>
 			<img src='src/assets/react-core-concepts.png' alt='Stylized atom' />
 			<h1>React Essentials</h1>
-			<p>Fundamental React concepts you will need for almost any app you are going to build!</p>
+			<p>
+				{/* 중괄호 사이에 자유롭게 자바스크립트 코드 실행 */}
+				{description}
+				React concepts you will need for almost any app you are going to build!
+			</p>
 		</header>
 	);
 }
