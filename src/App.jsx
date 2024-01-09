1ae259f5e918c2a87ae48e98f8d95c20c5873535
henny <kimhyein1105@gmail.com>
2024-01-09 22:25:32 +0900

    Props(속성) 대체 문법

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import reactImg from './assets/react-core-concepts.png';
 import componentsImg from './assets/components.png';
+import { CORE_CONCEPTS } from './data';
 
 const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
 
@@ -21,12 +22,12 @@ function Header() {
 	);
 }
 
-function CoreConcept(props) {
+function CoreConcept({ image, title, description }) {
 	return (
 		<li>
-			<img src={props.img} alt={props.title} />
-			<h3>{props.title}</h3>
-			<p>{props.description}</p>
+			<img src={image} alt={title} />
+			<h3>{title}</h3>
+			<p>{description}</p>
 		</li>
 	);
 }
@@ -39,10 +40,10 @@ function App() {
 				<section id='core-concepts'>
 					<h2>Core Concepts</h2>
 					<ul>
-						<CoreConcept title='Components' description='The core UI building block' img={componentsImg} />
-						<CoreConcept />
-						<CoreConcept />
-						<CoreConcept />
+						<CoreConcept {...CORE_CONCEPTS[0]} />
+						<CoreConcept title={CORE_CONCEPTS[1].title} description={CORE_CONCEPTS[1].description} image={CORE_CONCEPTS[1].image} />
+						<CoreConcept title={CORE_CONCEPTS[2].title} description={CORE_CONCEPTS[2].description} image={CORE_CONCEPTS[2].image} />
+						<CoreConcept title={CORE_CONCEPTS[3].title} description={CORE_CONCEPTS[3].description} image={CORE_CONCEPTS[3].image} />
 					</ul>
 				</section>
 			</main>
