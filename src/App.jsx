c7c9e3bd8dd4c8aa0b841992f054ff1393293390
henny <kimhyein1105@gmail.com>
2024-01-10 21:37:36 +0900

    파일에 컴포넌트 저장 및 좋은 프로젝트 구조 활용

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,6 @@
-import reactImg from './assets/react-core-concepts.png';
-import componentsImg from './assets/components.png';
 import { CORE_CONCEPTS } from './data';
-
-const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
-
-function genRandomInt(max) {
-	return Math.floor(Math.random() * (max + 1));
-}
-
-function Header() {
-	const description = reactDescriptions[genRandomInt(2)];
-	return (
-		<header>
-			<img src={reactImg} alt='Stylized atom' />
-			<h1>React Essentials</h1>
-			<p>
-				{description}
-				React concepts you will need for almost any app you are going to build!
-			</p>
-		</header>
-	);
-}
-
-function CoreConcept({ image, title, description }) {
-	return (
-		<li>
-			<img src={image} alt={title} />
-			<h3>{title}</h3>
-			<p>{description}</p>
-		</li>
-	);
-}
+import Header from './components/Header';
+import CoreConcept from './components/CoreConcept';
 
 function App() {
 	return (
@@ -41,9 +11,9 @@ function App() {
 					<h2>Core Concepts</h2>
 					<ul>
 						<CoreConcept {...CORE_CONCEPTS[0]} />
-						<CoreConcept title={CORE_CONCEPTS[1].title} description={CORE_CONCEPTS[1].description} image={CORE_CONCEPTS[1].image} />
-						<CoreConcept title={CORE_CONCEPTS[2].title} description={CORE_CONCEPTS[2].description} image={CORE_CONCEPTS[2].image} />
-						<CoreConcept title={CORE_CONCEPTS[3].title} description={CORE_CONCEPTS[3].description} image={CORE_CONCEPTS[3].image} />
+						<CoreConcept {...CORE_CONCEPTS[1]} />
+						<CoreConcept {...CORE_CONCEPTS[2]} />
+						<CoreConcept {...CORE_CONCEPTS[3]} />
 					</ul>
 				</section>
 			</main>
