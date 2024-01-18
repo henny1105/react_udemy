6a4ae81f885b62e720ac9542d106d3e115871335
henny <kimhyein1105@gmail.com>
2024-01-18 21:11:37 +0900

    여러 JSX 슬롯 활용법

--- src/components/Examples.jsx
+++ src/components/Examples.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import { EXAMPLES } from '../data.js';
 import TabButton from './TabButton.jsx';
 import Section from './Section.jsx';
+import Tabs from './Tabs.jsx';
 
 export default function Examples() {
 	const [selectedTopic, setSelectedTopic] = useState(null);
@@ -25,14 +26,18 @@ export default function Examples() {
 	}
 	return (
 		<Section title='Examples' id='examples'>
-			<h2>Example</h2>
-			<menu>
-				<TabButton isSelected={selectedTopic === 'components'} label='Components' onClick={() => handleSelect('components')} />
-				<TabButton isSelected={selectedTopic === 'jsx'} label='JSX' onClick={() => handleSelect('jsx')} />
-				<TabButton isSelected={selectedTopic === 'props'} label='Props' onClick={() => handleSelect('props')} />
-				<TabButton isSelected={selectedTopic === 'state'} label='State' onClick={() => handleSelect('state')} />
-			</menu>
-			{tabContent}
+			<Tabs
+				buttons={
+					<>
+						<TabButton isSelected={selectedTopic === 'components'} label='Components' onClick={() => handleSelect('components')} />
+						<TabButton isSelected={selectedTopic === 'jsx'} label='JSX' onClick={() => handleSelect('jsx')} />
+						<TabButton isSelected={selectedTopic === 'props'} label='Props' onClick={() => handleSelect('props')} />
+						<TabButton isSelected={selectedTopic === 'state'} label='State' onClick={() => handleSelect('state')} />
+					</>
+				}
+			>
+				{tabContent}
+			</Tabs>
 		</Section>
 	);
 }
