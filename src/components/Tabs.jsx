dbacf8b56546c1bcc9056c4e3667d7ac1d58fabd
henny <kimhyein1105@gmail.com>
2024-01-22 22:08:58 +0900

    기본 Prop(속성) 값 설정

--- src/components/Tabs.jsx
+++ src/components/Tabs.jsx
@@ -1,5 +1,4 @@
-export default function Tabs({ children, buttons, ButtonsContainer }) {
-	// buttonsContainer 속성은 반드시 받는 쪽 컴포넌트에서 커스텀 컴포넌트로 사용이 가능해야함
+export default function Tabs({ children, buttons, ButtonsContainer = 'menu' }) {
 	// const ButtonsContainer = buttonsContainer;
 	return (
 		<>
